6f75682f62d03c04db18e2135e6e990e8491fd13
profe1000 <soligboemmanuel@yahoo.com>
2023-08-18 07:44:00 +0100

    buisness section

--- src/pages/landingpages/Home/section/sectionbusiness.tsx
+++ src/pages/landingpages/Home/section/sectionbusiness.tsx
@@ -1,21 +1,41 @@
 import "./../Home.css";
 
-
 export const SectionBusiness = () => {
   return (
     <>
       <div
-        className="w3-col"
+        className="w3-col w3-center"
         style={{
           background: `url(${
             process.env.PUBLIC_URL + "/images/forindividualandbusiness.svg"
           })`,
           backgroundColor: "#f0f0f0",
-          paddingTop: "50px",
-          minHeight:"800px"
+          paddingTop: "180px",
+          minHeight: "800px",
         }}
       >
-        
+        <h1 className="sectionbuisnessheader">
+          For Individuals and Businesses
+        </h1>
+        <p className="sectionbuisnesscaption">
+          Join 200+ businesses using Eazipay's easy solution.
+        </p>
+
+        <p className="w3-hide-small w3-hide-medium">
+          <img
+            style={{ maxWidth: "100%" }}
+            alt="logo"
+            src={process.env.PUBLIC_URL + "/images/forbusinesscards.svg"}
+          />
+        </p>
+
+        <p className="sectionbuisnessfooter">
+          We are happy to answer your queries. Please, reach us at <br />
+          <span className="sectionbuisnessfooteremail">
+            hello@myeazipay.com
+          </span>
+          and expect our response shortly after.
+        </p>
       </div>
     </>
   );
