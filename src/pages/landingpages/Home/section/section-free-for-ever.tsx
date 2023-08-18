142a216d8187df2eac9f653b53705ec4bec48845
profe1000 <soligboemmanuel@yahoo.com>
2023-08-18 09:01:01 +0100

    section forever free

--- src/pages/landingpages/Home/section/section-free-for-ever.tsx
+++ src/pages/landingpages/Home/section/section-free-for-ever.tsx
@@ -1,7 +1,7 @@
+import { Link } from "react-router-dom";
 import "./../Home.css";
 
-
-export const SectionFreeForEver= () => {
+export const SectionFreeForEver = () => {
   return (
     <>
       <div
@@ -11,10 +11,66 @@ export const SectionFreeForEver= () => {
             process.env.PUBLIC_URL + "/images/forveverfreeminusdot.svg"
           })`,
           backgroundColor: "#f0f0f0",
-          paddingTop: "50px",
-          minHeight: "800px",
+          paddingBottom:"100px",
+          minHeight: "600px",
         }}
-      ></div>
+      >
+        <div className="w3-content">
+          <img
+            style={{ width: "80px", maxWidth: "100%" }}
+            alt="logo"
+            src={process.env.PUBLIC_URL + "/images/freeforeverdot.svg"}
+          />
+        </div>
+        <div className="w3-content" style={{ paddingTop: "50px" }}>
+          <div
+            className="w3-col l6 m12 s12"
+            style={{ paddingTop: "20px", paddingBottom: "50px" }}
+          >
+            <div className="w3-padding">
+              <h1 className="sectionforeverfreeheader">
+                Free forever <br /> for your
+                <span className="sectionforeverfreeheaderred"> salary</span>
+                <br />
+                <span className="sectionforeverfreeheaderred"> payments</span>
+              </h1>
+
+              <p style={{ paddingTop: "20px" }}>
+                <span className="downloadText">
+                  Subscribe to the Eazilife today
+                </span>
+                <br /> <br />
+                <Link className="heroDownloadBtn w3-btn" to="/">
+                  <img
+                    style={{ maxWidth: "100%" }}
+                    alt="heroImage"
+                    src={`${
+                      process.env.PUBLIC_URL + "/images/downloadapple.svg"
+                    }`}
+                  />
+                </Link>
+                &nbsp;
+                <Link className="heroDownloadBtn w3-btn" to="/">
+                  <img
+                    style={{ maxWidth: "100%" }}
+                    alt="heroImage"
+                    src={`${
+                      process.env.PUBLIC_URL + "/images/downloadgoogle.svg"
+                    }`}
+                  />
+                </Link>
+              </p>
+            </div>
+          </div>
+          <div className="w3-col l6 m12 s12">
+            <img
+              style={{ maxWidth: "100%" }}
+              alt="heroImage"
+              src={`${process.env.PUBLIC_URL + "/images/foreverfreeman.svg"}`}
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
