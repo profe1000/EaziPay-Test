cef24834dc1f6f46b1468f568a76d94fb1680078
profe1000 <soligboemmanuel@yahoo.com>
2023-08-18 08:32:25 +0100

    section how it work

--- src/pages/landingpages/Home/section/section-how-it-work.tsx
+++ src/pages/landingpages/Home/section/section-how-it-work.tsx
@@ -6,11 +6,38 @@ export const SectionHowItWork = () => {
       <div
         className=" w3-col w3-white"
         style={{
-          paddingTop: "50px",
+          paddingTop: "100px",
           minHeight: "800px",
         }}
       >
-       
+        <div className="w3-center">
+          <h1 className="sectionHowitworkHeader">How Eazipay Works</h1>
+          <p className="sectionHowitworkcaption">
+            Get started in 3 simple steps.
+          </p>
+        </div>
+
+        <div className="w3-content">
+          <div className="w3-col">
+            <div className="w3-col l6 s12 m12">
+              <div className="w3-col w3-center">
+                <img
+                  className="w3-round-xlarge"
+                  style={{ maxWidth: "100%", height: "700px" }}
+                  alt="How it works"
+                  src={process.env.PUBLIC_URL + "/images/howitworksphone.png"}
+                />
+              </div>
+            </div>
+            <div className="w3-col l6 s12 m12">
+              <img
+                style={{ maxWidth: "100%" }}
+                alt="How it works"
+                src={process.env.PUBLIC_URL + "/images/howitworkstep.png"}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
